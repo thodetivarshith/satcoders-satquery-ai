d49026c95b3e2549823fdd90807d9907910092f3
Banoth Akshaya <banothakshaya2316@gmail.com>
2026-09-09 21:45:09 +0530

    Week 1: Backend integration done

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
-# FastAPI application - Akshaya
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.routes.health import router as health_router
+from backend.routes.analyze import router as analyze_router
+app = FastAPI(title="SatQuery AI")
+app.add_middleware(CORSMiddleware, allow_origins=["*"],allow_methods=["*"],allow_headers=["*"],)
+app.include_router(health_router , prefix="/api")
+app.include_router(analyze_router , prefix="/api")
+@app.get("/")
+def root():
+    return{"message":"SatQuery AI Backend Running"}
--- backend/routes/analyze.py
+++ backend/routes/analyze.py
@@ -1,1 +1,12 @@
-# /analyze endpoint - Akshaya
+from fastapi import APIRouter, UploadFile, File, Form
+from PIL import Image
+import io
+router = APIRouter()
+@router.post("/api/analyze")
+async def analyze_image(file:UploadFile = File(...), query: str = Form(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    width,height = image.size
+    answer = f"your query was: '{query}' . Image size is{width}x{height}. The image shows a village with houses and agriculture fields."
+
+    return{"message": "Image received successfully","status": "success", "filename": file.filename, "query": query, "ai_answer": answer, "image_info": f"{width}x{height}"}
